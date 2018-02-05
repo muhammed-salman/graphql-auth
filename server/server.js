6ba222e427fc3f2b683d4985ae467b52478df164
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2018-02-05 13:18:51 +0530

    Mutations signup, login, logout

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://salman:salman@ds125058.mlab.com:25058/graph-auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
